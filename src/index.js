7cbf783b7b4ea77fa55dcbdbc0031ee4c8030bed
joaoqsl <joaojpmoreira25@gmail.com>
2020-07-22 23:37:12 -0300

    feat: install nodemon + nunjucks and start server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-import express from "express"
+const express = require('express')
+const path = require('path')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.render('./index.html')
+const port = 3333
+
+app.use(express.static('/public'))
+
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: app,
+    noCache: true,
 })
 
-app.listen(3333, () => {
-    console.log(`[*] Servidor rodando`)
+app.get('/', (req, res) => {
+    return res.render('index.html')
+})
+
+app.listen(port, () => {
+    console.log(`[*] Server is running at port: ${port}`)
+    console.log(`[*] Site is running at: http://localhost:${port}`)
 })
